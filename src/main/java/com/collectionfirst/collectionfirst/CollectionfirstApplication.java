86e162e2b34066a2cb63ce3db10c18f7f196973c
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-03-01 11:10:15 +0200

    Initial commit

---
+++ src/main/java/com/collectionfirst/collectionfirst/CollectionfirstApplication.java
@@ -0,0 +1,14 @@
+package com.collectionfirst.collectionfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionfirstApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/collectionfirst/collectionfirst/CollectionfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collectionfirst.collectionfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
