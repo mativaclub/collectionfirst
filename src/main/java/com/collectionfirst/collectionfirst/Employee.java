c8ef1e65d7c79d61e9a2be7e57f124b556115a1d
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-03-01 11:29:50 +0200

    homework

--- src/main/java/com/collectionfirst/collectionfirst/Employee.java
+++ src/main/java/com/collectionfirst/collectionfirst/Employee.java
@@ -4,8 +4,8 @@ import java.util.Objects;
 
 public class Employee {
 
-    String firstName;
-    String lastName;
+    private final String firstName;
+    private final String lastName;
 
     public Employee(String firstName, String lastName) {
         this.firstName = firstName;
